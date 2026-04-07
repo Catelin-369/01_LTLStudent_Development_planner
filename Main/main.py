86f280aeb9023961caeb48dfbe50368ba07661fa
Catelin-369 <caty.zaaiman@gmail.com>
2026-04-07 13:37:48 +0200

    Update main.py

--- Main/main.py
+++ Main/main.py
@@ -8,3 +8,10 @@ from UI.dashboard import edit_student_profile, show_student_profile
 
 edit_student_profile("STD001")
 show_student_profile("STD001")
+
+import tkinter as tk
+from UI.app import StudentApp
+
+root = tk.Tk()
+app = StudentApp(root)
+root.mainloop()
